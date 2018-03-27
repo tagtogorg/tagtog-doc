4e8223602daf99861d0435065e54c424d5ce9841
Jorge Campos <jorgecp@airpost.net>
2018-03-27 14:47:12 +0200

    Fix anchors handling in Chrome and Safari

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
   $(".tabs-menu a").click(function(event) {
-      event.preventDefault();
-      $(this).parent().addClass("current");
-      $(this).parent().siblings().removeClass("current");
-      var tab = $(this).attr("href");
-      $(tab).siblings(".tab-content").css("display", "none");
-      $(tab).fadeIn();
+    event.preventDefault();
+    $(this).parent().addClass("current");
+    $(this).parent().siblings().removeClass("current");
+    var tab = $(this).attr("href");
+    $(tab).siblings(".tab-content").css("display", "none");
+    $(tab).fadeIn();
   });
   var anchors = new AnchorJS();
+
   anchors.add();
+
+  var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  var isSafari = /Safari/.test(navigator.userAgent);
+  if (window.location.hash && (isChrome||isSafari)) {
+    setTimeout(function () {
+      var hash = window.location.hash;
+      window.location.hash = "";
+      window.location.hash = hash;
+    }, 300);
+  }
 });

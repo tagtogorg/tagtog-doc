50af8971c7848840c9b9340f3b39125d62dd3b5b
Jorge <jorgecp@airpost.net>
2019-04-26 11:07:00 +0200

    Create api_python_add_folder.py

---
+++ snippets/api_python_add_folder.py
@@ -0,0 +1,9 @@
+import requests
+
+tagtogAPIUrl = "https://www.tagtog.net/-api/settings/v1/folders/add"
+
+auth = requests.auth.HTTPBasicAuth(username='username', password='password')
+params = {'project':'projectName', 'owner': 'ownername'}
+payload = {'parentPath': 'pool', 'newFolderName':'myNewFolder'}
+response = requests.post(tagtogAPIUrl, auth=auth, json=payload, params=params)
+print(response.text)

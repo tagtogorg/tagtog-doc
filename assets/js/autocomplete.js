f1da207c51d30b0ebd7aa1353a0aee529141fd40
Uxío García Andrade <uxiog21@gmail.com>
2019-08-13 11:00:59 +0200

    remove dummy prints

--- assets/js/autocomplete.js
+++ assets/js/autocomplete.js
@@ -17,8 +17,6 @@ function autocomplete(inp, arr) {
         this.parentNode.appendChild(a);
         /*for each item in the array...*/
         for (i = 0; i < arr.length; i++) {
-          console.log(arr)
-          console.log(arr[i])
           /*check if the item starts with the same letters as the text field value:*/
           if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
             /*create a DIV element for each matching element:*/

df183635dae917070f0c1055a637d2f6a0954064
Uxío García Andrade <uxiog21@gmail.com>
2019-08-15 18:32:12 +0200

    minor

--- assets/js/autocomplete.js
+++ assets/js/autocomplete.js
@@ -92,5 +92,8 @@ function autocomplete(inp, arr) {
   /*execute a function when someone clicks in the document:*/
   document.addEventListener("click", function (e) {
       closeAllLists(e.target);
+      $(function() {
+        $(inp).keyup();
+    });
   });
   }

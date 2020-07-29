5bc0ce0629ac2c4b2e32a9feb604f6fab90a5575
Jorge <jorgecp@airpost.net>
2020-07-29 10:51:21 +0200

    Update api_python_import_pdf.py

--- snippets/api_python_import_pdf.py
+++ snippets/api_python_import_pdf.py
@@ -1,9 +1,9 @@
 import requests
 
-tagtogAPIUrl = "{{ page.api_document_url }}"
+tagtogAPIUrl = "https://www.tagtog.net/-api/documents/v1"
 
-auth = requests.auth.HTTPBasicAuth(username='{{ page.api_username }}', password='{{ page.api_pwd }}')
-params = {'project':'{{ page.api_project }}', 'owner': '{{ page.api_username }}', 'output':'ann.json'}
+auth = requests.auth.HTTPBasicAuth(username='YourUsername', password='YourPassword')
+params = {'project':'YourProjectName', 'owner': 'YourUsername', 'output':'ann.json'}
 #you can append more files to the list in case you want to upload multiple files
 files = [('file', open('files/document.pdf'))]
 response = requests.post(tagtogAPIUrl, params=params, auth=auth, files=files)

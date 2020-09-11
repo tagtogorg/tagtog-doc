41097be5754cbac513a771dfd4e7df95018e0d0b
Juan Miguel Cejuela <juanmi@tagtog.net>
2020-09-11 19:54:17 +0200

    change order of parameters (owner first, then project) and use double quotes CONT.

--- snippets/api_python_annotate_files.py
+++ snippets/api_python_annotate_files.py
@@ -3,7 +3,7 @@ import requests
 tagtogAPIUrl = "https://www.tagtog.net/-api/documents/v1"
 
 auth = requests.auth.HTTPBasicAuth(username="yourUsername", password="yourPassword")
-params = {'project': 'yourProject', 'owner': 'yourUsername', 'output': 'ann.json'}
+params = {"owner": "yourUsername", "project": "yourProject", "output": "ann.json"}
 # you can append more files to the list in case you want to upload multiple files
 files = [('file', open('files/text.txt'))]
 response = requests.post(tagtogAPIUrl, params=params, auth=auth, files=files)

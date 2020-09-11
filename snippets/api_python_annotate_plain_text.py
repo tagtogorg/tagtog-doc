dd7632731dd8cd5ff6cdeabb172e99b0fe4438e4
Juan Miguel Cejuela <juanmi@tagtog.net>
2020-09-11 19:24:47 +0200

    change order of parameters (owner first, then project) and use double quotes

--- snippets/api_python_annotate_plain_text.py
+++ snippets/api_python_annotate_plain_text.py
@@ -3,7 +3,7 @@ import requests
 tagtogAPIUrl = "https://www.tagtog.net/-api/documents/v1"
 
 auth = requests.auth.HTTPBasicAuth(username='yourUsername', password='yourPassword')
-params = {'project':'yourProject', 'owner': 'yourUsername', 'output':'ann.json'}
+params = {"owner": "yourUsername", "project": "yourProject", 'output':'ann.json'}
 payload = {'text': 'Antibody-dependent cellular cytotoxicity (ADCC), a key effector function for the clinical effectiveness of monoclonal antibodies, is triggered by the engagement of the antibody Fc domain with the Fcγ receptors expressed by innate immune cells such as natural killer (NK) cells and macrophages.'}
 response = requests.post(tagtogAPIUrl, params=params, auth=auth, data=payload)
 print(response.text)
--- snippets/api_python_search.py
+++ snippets/api_python_search.py
@@ -3,6 +3,6 @@ import requests
 tagtogAPIUrl = "https://www.tagtog.net/-api/documents/v1"
 
 auth = requests.auth.HTTPBasicAuth(username='yourUsername', password='yourPassword')
-params = {'project':'yourProject', 'owner': 'yourUsername', 'search':'entity:GGP:P02649'}
+params = {"owner": "yourUsername", "project": "yourProject", 'search':'entity:GGP:P02649'}
 response = requests.get(tagtogAPIUrl, params=params, auth=auth)
 print(response.text)
